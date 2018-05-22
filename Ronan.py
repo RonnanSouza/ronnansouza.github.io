faf034185e9536608cc6e25580b0e599e950c897
RonnanSouza <ronan.souza@ccc.ufcg.edu.br>
2018-05-21 22:00:34 -0300

    just a test for a presentation page

---
+++ Ronan.py
@@ -0,0 +1,26 @@
+class Ronan(object):
+	"""I'm a student of Computer Science at Federal University of Campina Grande (UFCG).
+	 During my high school, I made a technician computer maintenance course integrated with 
+	 the normal high school's grade. I have experience with python, java, linux, swift,
+	  web development."""
+
+
+
+	def technologies():
+		return [Android, Bootstrap]
+
+	def languages():
+		return [Portuguese: "Native", English: "Undestand and Speak", Spanish: "Undestend and Write"]
+
+	def contacts():
+		email = "ronan.souza at ccc dot ufcg dot edu dot br"
+		github = "/ronnansouza"
+		linkedIn = "/ronnansouza"
+		return email, github, linkedIn
+		
+
+
+
+
+
+		
